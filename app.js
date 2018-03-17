454776ea1de2ac73f88d0a6fdda67ad20fd1e1b1
bdevapatla <reachbp4@gmail.com>
2018-03-17 07:35:44 -0400

    assignment submission

---
+++ app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict'
+
+    angular.module('LunchCheck',[])
+        .controller('LunchCheckController',LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope){
+        $scope.message = "";
+        $scope.itemsList = "";
+        $scope.checkItemsLength = function () {
+            if(!$scope.itemsList){
+                $scope.message = "Please enter data first";
+                return;
+            }
+            var items = $scope.itemsList.split(",").filter(x => x);
+            var itemsLength = items.length;
+            if(itemsLength <=3 ){
+                $scope.message = "Enjoy!"
+            }
+            else {
+                $scope.message = "Too much!"
+            }
+        };
+    }
+})();
+
